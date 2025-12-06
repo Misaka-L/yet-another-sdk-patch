6646747a77b3fbdfd81fefd289887c7831f4b508
Misaka-L <lipww1234@foxmail.com>
2025-12-06 23:02:43 +0800

    feat: disable telemetry patch

---
+++ src/Packages/xyz.misakal.vpm.yet-another-sdk-patch/Editor/Patches/NoTelemetryPatch.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using AmplitudeSDKWrapper;
+using HarmonyLib;
+using UnityEngine;
+using VRC.Core;
+using YesPatchFrameworkForVRChatSdk.PatchApi;
+
+namespace YetAnotherPatchForVRChatSdk.Patches;
+
+internal sealed class NoTelemetryPatch : YesPatchBase
+{
+    public override string Id => "xyz.misakal.vpm.yet-another-sdk-patch.no-telemetry";
+    public override string DisplayName => "Disable SDK Telemetry";
+    public override string Description => "Disables all telemetry and analytics in the VRChat SDK.";
+    public override string Category => "Privacy?";
+
+    private readonly Harmony _harmony = new("xyz.misakal.vpm.yet-another-sdk-patch.no-telemetry");
+
+    public override void Patch()
+    {
+        _harmony.PatchAll(typeof(NoTelemetryPatch));
+    }
+
+    public override void UnPatch()
+    {
+        _harmony.UnpatchSelf();
+    }
+
+    [HarmonyPatch(typeof(AnalyticsInterface), nameof(AnalyticsInterface.Send))]
+    [HarmonyPrefix]
+    private static bool SendEventPrefix()
+    {
+        TryDeleteAmplitudeCache();
+        return false;
+    }
+
+    [HarmonyPatch(typeof(AmplitudeWrapper), "UpdateServer")]
+    [HarmonyPrefix]
+    private static bool UpdateServerPrefix()
+    {
+        TryDeleteAmplitudeCache();
+        return false;
+    }
+
+    private static void TryDeleteAmplitudeCache()
+    {
+        var cachePaths = GetAmplitudeCachePaths();
+        foreach (var cachePath in cachePaths)
+        {
+            if (!File.Exists(cachePath))
+                continue;
+
+            Debug.Log("[NoTelemetryPatch] Deleting Amplitude cache file: " + cachePath);
+            try
+            {
+                File.Delete(cachePath);
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning("[NoTelemetryPatch] Failed to delete Amplitude cache file: " + cachePath + "\n" + e);
+            }
+        }
+    }
+
+    private static string[] GetAmplitudeCachePaths()
+    {
+        return new[]
+        {
+            Path.Combine(Application.temporaryCachePath, "amplitude.cache"),
+            Path.Combine(Application.temporaryCachePath, "settings_com.amplitude")
+        };
+    }
+}
